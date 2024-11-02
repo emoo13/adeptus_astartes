cc0a46c80d3c426c3259f61fcc8809324622afb5
Estell Moore <estell.moore.13@gmail.com>
2024-11-01 19:38:42 -0600

    Removing unused files

--- index.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config("C:\\coding_projects\\adeptus-astartes\\.env")
-console.log(`Hello ${process.env.HELLO}`)
-// import { config } from 'dotenv';
-
-// config();
-// console.log(process.env.DB_URI);
