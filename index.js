416a26a9fe002564f6e352424a155b90390a72c7
Estell Moore <estell.moore.13@gmail.com>
2024-11-01 19:25:43 -0600

    Creating initial repo

---
+++ index.js
@@ -0,0 +1,6 @@
+require('dotenv').config("C:\\coding_projects\\adeptus-astartes\\.env")
+console.log(`Hello ${process.env.HELLO}`)
+// import { config } from 'dotenv';
+
+// config();
+// console.log(process.env.DB_URI);
